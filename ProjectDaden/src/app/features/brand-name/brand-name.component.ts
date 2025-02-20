42ecf52f78a8c2e9efa801b49c57949ca3081cdf
danny <danny_karouw@hotmail.com>
2025-02-20 23:06:31 +0100

    Dynamic modular daden table, needs styling, test on brandName page

--- ProjectDaden/src/app/features/brand-name/brand-name.component.ts
+++ ProjectDaden/src/app/features/brand-name/brand-name.component.ts
@@ -1,26 +1,24 @@
 import { Component, signal, TemplateRef } from '@angular/core';
 import { DadenTableComponent } from '../../shared/components/daden-table/daden-table.component';
-import { JsonPipe } from '@angular/common';
+import { CommonModule, JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-brand-name',
-  imports: [DadenTableComponent, JsonPipe],
+  imports: [DadenTableComponent, JsonPipe, CommonModule],
   templateUrl: './brand-name.component.html',
   styleUrl: './brand-name.component.scss',
   standalone: true,
 })
 export class BrandNameComponent {
-  tableData = signal([
-    { name: 'John', age: 25, country: 'USA' },
-    { name: 'Anna', age: 22, country: 'Canada' },
-    { name: 'Mike', age: 30, country: 'UK' },
+  tableData = signal<{name: string, age: number, country: string, mijnKlets: string}[]>([
+    { name: 'John', age: 25, country: 'USA', mijnKlets: 'Klets' },
+    { name: 'Anna', age: 22, country: 'Canada', mijnKlets: 'Klets2' },
+    { name: 'Mike', age: 30, country: 'UK', mijnKlets: 'Klets3' },
   ]);
 
-  tableColumns = signal(['Name', 'Age', 'Country']);
+  tableColumns = signal(['Name', 'Age', 'Country', 'Mijn Klets']);
 
   additionalContext = signal({
     showAge: true,
   });
-
-  rowTemplate!: TemplateRef<any>;
 }
--- ProjectDaden/src/app/shared/components/daden-table/daden-table.component.ts
+++ ProjectDaden/src/app/shared/components/daden-table/daden-table.component.ts
@@ -1,15 +1,18 @@
-import { Component, input, TemplateRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, input, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'app-daden-table',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './daden-table.component.html',
   styleUrl: './daden-table.component.scss',
   standalone: true
 })
 export class DadenTableComponent {
-  data = input<{name: string, age: number, country: string}[]>([]);
+
+  data = input<{ name: string, age: number, country: string, mijnKlets: string }[]>([]);
   columns = input<string[]>([]);
   rowTemplate = input<TemplateRef<any> | null>(null);
   context = input<any>({});
+
 }
