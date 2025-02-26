9db9cfa92909b40b0d59bd8dc5cda6aad1b3aca7
MudHolland <103423480+MudHolland@users.noreply.github.com>
2025-02-26 09:37:48 +0100

    added card image bool and slider styling

--- ProjectDaden/src/app/shared/components/daden-card/daden-card.component.ts
+++ ProjectDaden/src/app/shared/components/daden-card/daden-card.component.ts
@@ -1,8 +1,9 @@
 import { Component, input, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-daden-card',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './daden-card.component.html',
   styleUrl: './daden-card.component.scss',
   standalone: true
@@ -11,6 +12,7 @@ export class DadenCardComponent {
   title = input<string>('Default Title');
   description = input<string>('Default description');
   imageUrl = input<string>('someImgRef');
+  showImage = input<boolean>(true);
 
   constructor() {}
 }
