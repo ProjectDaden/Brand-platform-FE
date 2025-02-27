e191da13685440d940b78845c69e215d9176605c
MudHolland <103423480+MudHolland@users.noreply.github.com>
2025-02-27 17:08:52 +0100

    nav order, typography page, archetype to color and components

--- ProjectDaden/src/app/features/section-typography/section-typography.component.ts
+++ ProjectDaden/src/app/features/section-typography/section-typography.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-section-typography',
-  imports: [],
+  imports: [DadenDropdownComponent],
   templateUrl: './section-typography.component.html',
   styleUrl: './section-typography.component.scss',
   standalone: true
