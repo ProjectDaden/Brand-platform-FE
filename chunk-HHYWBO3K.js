9de60535f02ac10729c7e75f7b15a580a1cf9f2e
MudHolland <MudHolland@users.noreply.github.com>
2025-04-29 14:55:54 +0000

    deploy: 8fe51e3486c9e857ef5ac162caf5708bf7a77214

---
+++ chunk-HHYWBO3K.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-7JM5BUSG.js";import"./chunk-SOCDX6SF.js";import"./chunk-AQ72KVWW.js";import"./chunk-TZGC5O4P.js";export{a as ToolColorPickerComponent};
---
+++ chunk-V5X3SSLS.js
@@ -0,0 +1,1 @@
+import{Ka as t,ac as a,bc as i,db as o}from"./chunk-TZGC5O4P.js";var r=class n{static \u0275fac=function(e){return new(e||n)};static \u0275cmp=t({type:n,selectors:[["app-print-offline-material"]],decls:2,vars:0,template:function(e,f){e&1&&o(0,"daden-header")(1,"daden-page-footer")},dependencies:[a,i],encapsulation:2})};export{r as PrintOfflineMaterialComponent};
